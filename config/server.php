aa1fc82b18719c959fae83ed9e3bdb5f417b12e0
luismiguel2301 <luismiguel2301@gmail.com>
2024-07-24 23:23:19 -0400

    Update server.php

--- config/server.php
+++ config/server.php
@@ -1,8 +1,8 @@
 <?php
-const DB_SERVER="192.168.1.254";
-const DB_NAME="crud1";
-const DB_USER="root";
-const DB_PASS="";
+const DB_SERVER="sql104.infinityfree.com";
+const DB_NAME="if0_36851971_cur";
+const DB_USER="if0_36851971";
+const DB_PASS="EkNTZM6GPjuFAwK";
 
 const METHOD="AES-256-CBC";
 const SECRET_KEY='$CRUD@2024';
